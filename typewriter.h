4263a772c0a5ab47d46d5b54e4cadb97e653a85a
Jakub Ducho <87933202+Ghost9887@users.noreply.github.com>
2026-06-21 21:29:40 +0200

    Update typewriter.h

--- typewriter.h
+++ typewriter.h
@@ -6,7 +6,7 @@
 #include <thread>
 #include <chrono>
 
-void typewriter_effect(const std::string& str, float pause_seconds) {
+void typewriter_effect(const std::string &str, float pause_seconds) {
     for (char c : str) {
         std::cout << c << std::flush;
         std::this_thread::sleep_for(std::chrono::duration<float>(pause_seconds));
